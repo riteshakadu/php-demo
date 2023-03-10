4e5a81b1bf78b4783fcb2a31bf04337aa6e4f3d5
Ubuntu <ubuntu@ip-172-31-0-40.ap-south-1.compute.internal>
2023-03-10 10:38:45 +0000

    updated php application

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+define("DB_DRIVER", "mysql");
+define("DB_HOST", "database-1.c9ltne2fkcqr.ap-south-1.rds.amazonaws.com");
+define("DB_USER", "admin");
+define("DB_PASS", "abcd1234");
+define("DB_DATABASE", "test_db");
+define("DB_CHARSET", "utf8");
+
+/*
+return array(
+    "driver"    =>"mysql",
+    "host"      =>"localhost",
+    "user"      =>"root",
+    "pass"      =>"",
+    "database"  =>"mvc1",
+    "charset"   =>"utf8"
+);
+*/
+
+?>
