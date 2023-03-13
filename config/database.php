3d75d1841bde437e687a0754116d493d7e38c937
cloud-dev-user <70812553+cloud-dev-user@users.noreply.github.com>
2023-03-13 13:44:56 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
 
 define("DB_DRIVER", "mysql");
-define("DB_HOST", "database-1.c9ltne2fkcqr.ap-south-1.rds.amazonaws.com");
+define("DB_HOST", "localhost");
 define("DB_USER", "admin");
-define("DB_PASS", "abcd1234");
-define("DB_DATABASE", "test_db");
+define("DB_PASS", "");
+define("DB_DATABASE", "");
 define("DB_CHARSET", "utf8");
 
 /*
